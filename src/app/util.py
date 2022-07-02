d5fd3174fe44625ba1b018c6f14cc8fccb0082ee
Etienne Posthumus <eposthumus@gmail.com>
2022-07-02 17:40:52 +0100

    Updated fieldnames

--- src/app/util.py
+++ src/app/util.py
@@ -4,14 +4,14 @@ import json
 def CIT_A(obj_json, cit_list):
     obj = json.loads(obj_json)
     cit1 = set(cit_list.split(" "))
-    cit2 = set(obj.get("CIT_ID", []))
+    cit2 = set(obj.get("CIT", []))
     if cit1 <= cit2:
         return True
 
 
 def CIT_O(obj_json, cit_list):
     obj = json.loads(obj_json)
-    cit2 = set(obj.get("CIT_ID", []))
+    cit2 = set(obj.get("CIT", []))
     for cit in set(cit_list.split(" ")):
         if cit in cit2:
             return True
@@ -19,7 +19,7 @@ def CIT_O(obj_json, cit_list):
 
 def CIT_N(obj_json, cit_list):
     obj = json.loads(obj_json)
-    cit2 = set(obj.get("CIT_ID", []))
+    cit2 = set(obj.get("CIT", []))
     for cit in set(cit_list.split(" ")):
         if cit in cit2:
             return False
